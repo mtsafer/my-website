0a5d3a8bad5bd5d4b6b1633ba9f98d4ceabdfd96
Martin Safer <mtsafer@gmail.com>
2016-09-08 15:01:49 -0700

    missed a line, oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  host = 'murmuring-lowlands-26772.herokuapp.com'
+  host = 'fierce-refuge-53778.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
